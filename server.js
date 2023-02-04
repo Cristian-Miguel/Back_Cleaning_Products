cdc23c460477a8ca13042c35cc490e26f28fbbf8
cristian miguel marin barrera <cristian_enano_99@hotmail.com>
2023-02-04 10:49:40 -0600

    changed elements

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express'),
+    server_config = require('config'),
     app = express(),
-    port = process.env.PORT || 8080
+    port = process.env.PORT || server_config.get('server.port')
+    // sesion = require('express-sesion')
+
 app.listen(port, () => {
     const datetime = new Date()
     const message = "Server enable on Port: " + port + " in the date: " + datetime
