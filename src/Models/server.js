e14b2548d79164b7a0bea34985db91938ca1a6a7
cristian miguel marin barrera <cristian_enano_99@hotmail.com>
2023-04-28 13:11:03 -0600

    Change the format of the route in the server file

--- src/Models/server.js
+++ src/Models/server.js
@@ -7,6 +7,16 @@ class Server {
     constructor() {
         this.app = express()
         this.port = process.env.PORT || server_config.get( 'server.port' )
+        this.paths = {
+           Auth : '/Auth',
+           User : '/User',
+           Sell : '/Sell',
+           Customer : '/Customer',
+           Product : '/Product',
+           Employee : '/Employee',
+           Inventory : '/Inventory',
+           Supplier : '/Supplier',
+        }
         this.middlewares ()
         this.routes()
     }
@@ -25,14 +35,14 @@ class Server {
     }
 
     routes () {
-        this.app.use( '/Auth', require( '../Routes/Auth_Routes' ) )
-        this.app.use( '/User', require( '../Routes/User_Routes' ) )
-        this.app.use( '/Sell', require( '../Routes/Sell_Products_Routes' ) )
-        this.app.use( '/Customer',  require( '../Routes/Customer_Routes' ) )
-        this.app.use( '/Product',  require( '../Routes/Products_Routes' ) )
-        this.app.use( '/Employee',  require( '../Routes/Employee_Routes' ) )
-        this.app.use( '/Inventory',  require( '../Routes/Inventory_Routes' ) )
-        this.app.use( '/Supplier',  require( '../Routes/Supplier_Routes' ) )
+        this.app.use( this.paths.Auth, require( '../Routes/Auth_Routes' ) )
+        this.app.use( this.paths.User, require( '../Routes/User_Routes' ) )
+        this.app.use( this.paths.Sell, require( '../Routes/Sell_Products_Routes' ) )
+        this.app.use( this.paths.Customer,  require( '../Routes/Customer_Routes' ) )
+        this.app.use( this.paths.Product,  require( '../Routes/Products_Routes' ) )
+        this.app.use( this.paths.Employee,  require( '../Routes/Employee_Routes' ) )
+        this.app.use( this.paths.Inventory,  require( '../Routes/Inventory_Routes' ) )
+        this.app.use( this.paths.Supplier,  require( '../Routes/Supplier_Routes' ) )
     }
 
     listen () {
