e54a87c1787168cca6a66ba84eee1d5c36699120
cristian miguel marin barrera <cristian_enano_99@hotmail.com>
2023-05-03 22:45:01 -0600

    Added the route of insert in the formula file

--- src/Routes/Formula_Routes.js
+++ src/Routes/Formula_Routes.js
@@ -13,6 +13,25 @@ router.get( '/list', [
     accessRol(1,6),
     validate_data
 ], FormulaGet )
+
+router.post( '/insert', [
+    validate_token,
+    accessRol(1,6),
+    check( 'Id_Formula', 'Id formula required' ).not().isEmpty(),
+    check( 'Id_Formula', 'Id formula required' ).isNumeric(),
+    check( 'Id_Formula', 'Id formula required' ).custom( Exist_Formula ),
+    check( 'Id_Raw_Material', 'Id_Raw_Material number required' ).not().isEmpty(),
+    check( 'Id_Raw_Material', 'Id_Raw_Material number required' ).isNumeric(),
+    check( 'Id_Raw_Material').custom( Exist_Raw_Material ),
+    check( 'Total_Cost', 'Total cost required' ).not().isEmpty(),
+    check( 'Total_Cost', 'Total cost required' ).isFloat(),
+    check( 'Quantity_Made', 'Quantity made required' ).not().isEmpty(),
+    check( 'Quantity_Made', 'Quantity made required' ).isNumeric(),
+    check( 'Unit_Cost', 'Unit cost required' ).not().isEmpty(),
+    check( 'Unit_Cost', 'Unit cost required' ).isFloat(),
+    validate_data
+], FormulaInsert )
+
 //--------- Routes ---------\\
 
 module.exports = router
